d42fc9c0bbb77d3cbaa56c430759a8371aff72fd
Silvia Martinez <sil@silvia.com>
2022-02-20 00:39:00 +0100

    Created a component OneRobot

---
+++ src/components/OneRobot/OneRobot.js
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+
+const RobotCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #1a659e;
+  flex-wrap: wrap;
+  margin: 10px;
+  border-radius: 25px;
+  border: 3px solid grey;
+  width: 480px;
+  img {
+    height: 220px;
+  }
+`;
+
+const Nombre = styled.h2`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: #ff6b35;
+`;
+
+const Caracteristicas = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #f7c59f;
+  font-size: 15px;
+`;
+
+const OneRobot = ({
+  robot: {
+    name,
+    image,
+    caractheristics: { velocity, resistence, creation },
+  },
+}) => {
+  return (
+    <>
+      <RobotCard>
+        <Nombre>{name}</Nombre>
+        <img src={image} alt="imagen robot" />
+        <Caracteristicas>
+          <p>Velocidad:{velocity}</p>
+          <p>Resistencia:{resistence}</p>
+          <p>Fecha de creacion:{creation}</p>
+        </Caracteristicas>
+      </RobotCard>
+    </>
+  );
+};
+
+export default OneRobot;
