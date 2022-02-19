3561addf3d40c4d2882ef4d0a1c57653b32e1808
Silvia Martinez <sil@silvia.com>
2022-02-19 23:20:16 +0100

    Update Robot test and handlers

--- src/components/Robot/Robot.test.js
+++ src/components/Robot/Robot.test.js
@@ -1,13 +1,22 @@
-import { screen, render } from "@testing-library/react";
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../setupTests";
 
 import Robot from "./Robot";
 
 describe("Given a Robot component", () => {
   describe("When it is instancied", () => {
     test("It should render a heading", () => {
-      render(<Robot />);
+      const robot = {
+        id: 1,
+        name: "Pepe",
+        image: "unafoto",
+        velocity: "3",
+        resistence: "7",
+        creation: "2019",
+      };
+      renderWithProviders(<Robot robot={robot} />);
 
-      const heading = screen.getByRole("heading", { name: "Nombre" });
+      const heading = screen.getByRole("heading", { name: "Pepe" });
 
       expect(heading).toBeInTheDocument();
     });
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,21 +2,32 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.get(process.env.REACT_APP_API_URL, (req, res, ctx) => {
-    console.log("hi");
     return res(
       ctx.status(200),
       ctx.json([
         {
           id: 1,
-          nombre: "Pepe",
+          name: "Pepe",
+          image: "unafoto",
+          velocity: "3",
+          resistence: "7",
+          creation: "2019",
         },
         {
           id: 2,
-          nombre: "Pipo",
+          name: "Pipo",
+          image: "unafoto",
+          velocity: "3",
+          resistence: "7",
+          creation: "2020",
         },
         {
           id: 3,
-          nombre: "Pupe",
+          name: "Pupe",
+          image: "unafoto",
+          velocity: "3",
+          resistence: "7",
+          creation: "2010",
         },
       ])
     );
