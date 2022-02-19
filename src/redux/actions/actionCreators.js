ee21d6e40a039ae9aee36f6b255fc92c16d36dfb
Silvia Martinez <sil@silvia.com>
2022-02-20 00:41:24 +0100

    Added new actionCreator

--- src/redux/actions/actionCreators.js
+++ src/redux/actions/actionCreators.js
@@ -4,3 +4,8 @@ export const loadRobotsActions = (robots) => ({
   type: actionTypes.loadRobots,
   robots,
 });
+
+export const loadOneRobot = (id) => ({
+  type: actionTypes.loadOneRobot,
+  id,
+});
