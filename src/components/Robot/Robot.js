9070b9e85a0dd22b86b154bab2cb070efb6d0b07
Silvia Martinez <sil@silvia.com>
2022-02-19 13:38:45 +0100

    Added style to component Robot

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -1,11 +1,41 @@
+import styled from "styled-components";
+
+const RobotCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: gold;
+  flex-wrap: wrap;
+`;
+
+const Nombre = styled.h2`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  color: red;
+`;
+
+const Caracteristicas = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: violet;
+  font-size: 15px;
+`;
+
 const Robot = ({ nombre, imagen, velocidad, resistencia, creacion }) => {
   return (
     <>
-      <h2>{nombre}</h2>
-      <img src={imagen} alt="imagen robot" />
-      <p>Velocidad:{velocidad}</p>
-      <p>Resistencia:{resistencia}</p>
-      <p>Fecha de creacion:{creacion}</p>
+      <RobotCard>
+        <Nombre>{nombre}Nombre</Nombre>
+        <img src={imagen} alt="imagen robot" />
+        <Caracteristicas>
+          <p>Velocidad:{velocidad}</p>
+          <p>Resistencia:{resistencia}</p>
+          <p>Fecha de creacion:{creacion}</p>
+        </Caracteristicas>
+      </RobotCard>
     </>
   );
 };
