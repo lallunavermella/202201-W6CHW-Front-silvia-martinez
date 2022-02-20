e493ec4e618d90725bce45edcab0dd0ff14b720c
Silvia Martinez <sil@silvia.com>
2022-02-20 20:04:38 +0100

    Add test to delete thunk

--- src/redux/thunks/thunks.test.js
+++ src/redux/thunks/thunks.test.js
@@ -1,4 +1,4 @@
-import { loadRobotsThunks } from "./thunks";
+import { deleteRobotThunks, loadRobotsThunks } from "./thunks";
 
 describe("Given a load thunk function", () => {
   describe("When it's invoked", () => {
@@ -6,6 +6,34 @@ describe("Given a load thunk function", () => {
       const dispatch = jest.fn();
 
       await loadRobotsThunks(dispatch);
+
+      expect(dispatch).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a delete thunk function", () => {
+  describe("When it's invoked with id=10", () => {
+    test("it shouldn't call a dispatch", async () => {
+      const dispatch = jest.fn();
+      const id = 10;
+
+      const deleteThunk = deleteRobotThunks(id);
+
+      await deleteThunk(dispatch);
+
+      expect(dispatch).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("When it's invoked a with id=2", () => {
+    test("it should call a dispatch", async () => {
+      const dispatch = jest.fn();
+      const id = 2;
+
+      const deleteThunk = deleteRobotThunks(id);
+
+      await deleteThunk(dispatch);
 
       expect(dispatch).toHaveBeenCalled();
     });
