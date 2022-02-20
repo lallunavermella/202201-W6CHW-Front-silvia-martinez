66dec366bdcca57ef6669aae0f46f99f1cf48bd1
Silvia Martinez <sil@silvia.com>
2022-02-20 19:18:57 +0100

    Add actionTypes.deleteRobot at robotsReducer test

--- src/redux/reducers/robotsReducers.test.js
+++ src/redux/reducers/robotsReducers.test.js
@@ -1,7 +1,7 @@
 import actionTypes from "../actions/actionTypes";
 import robotsReducers from "./robotsReducers";
 
-describe("Given robotsReducer", () => {
+describe("Given robotsReducers", () => {
   describe("When given a current obejct and action", () => {
     test("Then should return a newRobots with current objects and actions", () => {
       const robots = [1, 2, 3];
@@ -16,7 +16,7 @@ describe("Given robotsReducer", () => {
       expect(newRobots).toStrictEqual(robots);
     });
   });
-  describe("When given a currentlist and a action that is not in actionCreators", () => {
+  describe("When given a currentRobots and a action that is not in actionCreators", () => {
     test("Then should return the current list because its using default statement", () => {
       const currentlist = [23, 34, 47, 56];
       const action = { type: "not-work" };
@@ -24,6 +24,24 @@ describe("Given robotsReducer", () => {
       const receivedList = robotsReducers(currentlist, action);
 
       expect(receivedList).toEqual(currentlist);
+    });
+  });
+  describe("When given a delete action", () => {
+    test("Then should return the list minus the corresponding with the id", () => {
+      const currentRobot = [
+        { name: "Pepe", _id: 1 },
+        { name: "Pipo", _id: 2 },
+      ];
+
+      const expectedRobots = [{ name: "Pipo", _id: 2 }];
+      const action = {
+        type: actionTypes.deleteRobot,
+        id: 1,
+      };
+
+      const newRobots = robotsReducers(currentRobot, action);
+
+      expect(newRobots).toEqual(expectedRobots);
     });
   });
 });
