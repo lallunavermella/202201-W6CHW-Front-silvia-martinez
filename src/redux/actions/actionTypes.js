6ac917ad35c70b8f75a579908c150327f91a005a
Silvia Martinez <sil@silvia.com>
2022-02-20 21:42:03 +0100

    Created a createdRobot in actionTypes

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -2,6 +2,7 @@ const actionTypes = {
   loadRobots: "load-robots",
   loadOneRobot: "load-one-robot",
   deleteRobot: "delete-robot",
+  createRobot: "create-robot",
 };
 
 export default actionTypes;
